dd0498b8a75d971bf851c8a67bc8baa1fcc8185d
Kim Princis <kimprincis@Mac.lan>
2025-12-18 11:09:22 +0100

    Implement custom object integration and views

--- index.js
+++ index.js
@@ -1,71 +1,94 @@
-const express = require('express');
-const axios = require('axios');
-const app = express();
+require('dotenv').config()
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const express = require('express')
+const axios = require('axios')
+const path = require('path')
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const app = express()
+const PORT = process.env.PORT || 3000
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+const HUBSPOT_TOKEN = process.env.HUBSPOT_PRIVATE_APP_TOKEN
+const HUBSPOT_BASE_URL = 'https://api.hubapi.com'
 
-// * Code for Route 1 goes here
+const CUSTOM_OBJECT = '2-54738442'
 
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+const PROPERTIES = [
+  { name: 'name', label: 'Name' },
+  { name: 'species', label: 'Species' },
+  { name: 'color', label: 'Color' }
+]
 
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
 
-// * Code for Route 3 goes here
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
 
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
+app.get('/', async (req, res) => {
+  try {
+    const propertiesParam = PROPERTIES.map(p => p.name).join(',')
 
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
+    const response = await axios.get(
+      `${HUBSPOT_BASE_URL}/crm/v3/objects/${CUSTOM_OBJECT}?properties=${propertiesParam}`,
+      {
+        headers: {
+          Authorization: `Bearer ${HUBSPOT_TOKEN}`
         }
-    }
+      }
+    )
 
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
+    const records = response.data.results || []
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
+    res.render('homepage', {
+      title: 'Homepage | Integrating With HubSpot I Practicum',
+      properties: PROPERTIES,
+      records
+    })
+  } catch (error) {
+    console.error(error.response?.data || error.message)
+    res.status(500).send('Could not load data from HubSpot')
+  }
+})
 
-});
-*/
+app.get('/update-cobj', (req, res) => {
+  res.render('updates', {
+    title: 'Update Custom Object Form | Integrating With HubSpot I Practicum',
+    properties: PROPERTIES
+  })
+})
 
+app.post('/update-cobj', async (req, res) => {
+  try {
+    const payloadProperties = {}
 
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+    PROPERTIES.forEach(prop => {
+      if (req.body[prop.name]) {
+        payloadProperties[prop.name] = req.body[prop.name]
+      }
+    })
+
+    await axios.post(
+      `${HUBSPOT_BASE_URL}/crm/v3/objects/${CUSTOM_OBJECT}`,
+      {
+        properties: payloadProperties
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${HUBSPOT_TOKEN}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+
+    res.redirect('/')
+  } catch (error) {
+    console.error(error.response?.data || error.message)
+    res.status(500).send('Could not create record in HubSpot')
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
+})
